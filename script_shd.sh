dab834427a87717f3b6852e4f7fc0c9a233fccca
mehrankazemi1122 <35174945+mehrankazemi1122@users.noreply.github.com>
2024-07-02 22:24:05 +0330

    Update script_shd.sh

--- script_shd.sh
+++ script_shd.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Function to send notification to Discord with a delay
+send_discord_notification() {
+    local content="$1"
+    local webhook_url="https://discord.com/api/webhooks/1257309085351542786/ijyiMHCcmhafcTqgumjZkLFLJAOuPvvgTqNdas3Rg3v1uHr8Qf07TNLq_Oo1JYajBlM8"
+    
+    # Add a delay of 5 seconds before sending each notification
+    sleep 5
+    
+    # Send the notification
+    curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"$content\"}" "$webhook_url"
+}
+
 # Check if arguments are provided
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <wordlist_file> <website_address>"
@@ -15,14 +27,13 @@ shuffledns -w "$wordlist_file" -d "$website_address" -r resolvers.txt -m massdns
 
 # Step 2: Remove duplicates from output file
 cat "output.${website_address}.txt" | sort -u | sponge "output.${website_address}.txt"
-cat "prev_output.${website_address}.txt" | sort -u | sponge "prev_output.${website_address}.txt"
 
 # Step 3: Compare with previous output and send additions to Discord
 added_subdomains=$(comm -23 "output.${website_address}.txt" "prev_output.${website_address}.txt")
 if [ -n "$added_subdomains" ]; then
     echo "subdomains Added:"
     echo "$added_subdomains" | while IFS= read -r line; do
-        curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"subdomains Added: $line\"}" https://discord.com/api/webhooks/1257309085351542786/ijyiMHCcmhafcTqgumjZkLFLJAOuPvvgTqNdas3Rg3v1uHr8Qf07TNLq_Oo1JYajBlM8
+        send_discord_notification "subdomains Added: $line"
     done
 fi
 
@@ -31,10 +42,10 @@ deleted_subdomains=$(comm -23 "prev_output.${website_address}.txt" "output.${web
 if [ -n "$deleted_subdomains" ]; then
     echo "subdomains Deleted:"
     echo "$deleted_subdomains" | while IFS= read -r line; do
-        curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"subdomains Deleted: $line\"}" https://discord.com/api/webhooks/1257309085351542786/ijyiMHCcmhafcTqgumjZkLFLJAOuPvvgTqNdas3Rg3v1uHr8Qf07TNLq_Oo1JYajBlM8
+        send_discord_notification "subdomains Deleted: $line"
     done
 fi
 
-# Step 5: Replace previous output file with current output file
+# Step 6: Replace previous output file with current output file
 rm -f "prev_output.${website_address}.txt"
 mv "output.${website_address}.txt" "prev_output.${website_address}.txt"
