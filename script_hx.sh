e4bca9d9971bfa16a30c93fc157d899906fa922c
mehrankazemi1122 <35174945+mehrankazemi1122@users.noreply.github.com>
2024-07-03 20:56:26 +0330

    Update script_hx.sh

--- script_hx.sh
+++ script_hx.sh
@@ -5,12 +5,13 @@ if [ $# -ne 1 ]; then
     echo "Usage: $0 <website_address>"
     exit 1
 fi
-
+ echo "file executed !" 
 # Assign argument to variable
 website_address="$1"
 
 # Function to send notification to Discord with a delay
 send_discord_notification() {
+    echo "sending function start !"
     local content="$1"
     local webhook_url="https://discord.com/api/webhooks/1257309085351542786/ijyiMHCcmhafcTqgumjZkLFLJAOuPvvgTqNdas3Rg3v1uHr8Qf07TNLq_Oo1JYajBlM8"
 
@@ -22,10 +23,12 @@ send_discord_notification() {
 
     # Send the notification
     curl -H "Content-Type: application/json" -X POST -d "$json_payload" "$webhook_url"
+    echo "sending function ends"
 }
 
 
 # Step 1: Run httpx command and save results
+echo "main command is executing"
 cat "prev_output.${website_address}.txt" | dnsx -silent | while read line; do
     echo "$line" | httpx -silent -follow-host-redirects -title -status-code -cdn -tech-detect \
         -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:108.0) Gecko/20100101 Firefox/108.0" \
@@ -34,10 +37,13 @@ done | sort -u > httpx_res.txt
 
 # Step 2: Compare with previous httpx results and send changes to Discord
 if [ -f "prev_httpx_res.txt" ]; then
+    echo "first if approved !"
     changes=$(comm -23 httpx_res.txt prev_httpx_res.txt)
     if [ -n "$changes" ]; then
+        echo "second if approved !"
         echo "services changes:"
         while IFS= read -r line; do
+            echo "first while approved !"
             prev_line=$(grep -F "$line" prev_httpx_res.txt)
             echo "previous : $prev_line"
             echo "updated : $line"
@@ -49,3 +55,4 @@ fi
 # Step 3: Replace previous httpx results with current results
 rm -f "prev_httpx_res.txt"
 mv "httpx_res.txt" "prev_httpx_res.txt"
+echo "script ends successfully"
